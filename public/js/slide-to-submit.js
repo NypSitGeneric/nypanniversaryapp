babd90c4d769725bd118b6de9e91a8e86f5e367b
lee_keat_kean <lee_keat_kean@nyp.edu.sg>
2022-08-02 15:55:12 +0800

    Update slide-to-submit.js

--- public/js/slide-to-submit.js
+++ public/js/slide-to-submit.js
@@ -109,7 +109,7 @@
 				var orig = e.originalEvent;
 				if (window.innerHeight > window.innerWidth) {
 					$(this).addClass('slide-submit-dragging').css({
-						down: orig.changedTouches[0].pageY - offset.y
+						top: orig.changedTouches[0].pageY - offset.y
 					});
 				}else {
 					$(this).addClass('slide-submit-dragging').css({
